6a58315783b91dc11101d0151b71e21899dc6e83
“steamboatkaty” <Katie@sky.com>
2026-03-29 15:53:38 +0100

    Added postcode input and improved error handling when an incorrect postcode is written

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 import requests
 
-URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/SE83NB"
+BASE_URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/"
 
-def fetch_restaurants():
+def fetch_restaurants(postcode):
+    url = BASE_URL + postcode.replace(" ", "")
+
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
-    response = requests.get(URL, headers=headers)
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         raise Exception("API request failed")
--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from api import fetch_restaurants
 from parser import format_restaurant
 
 def main():
-    restaurants = fetch_restaurants()
-    top_10 = restaurants[:10]
+    postcode = input("Enter a UK postcode: ")
 
-    for restaurant in top_10:
-        formatted = format_restaurant(restaurant)
+    try:
+        restaurants = fetch_restaurants(postcode)
+       
+        if not restaurants:
+            print("No restaurants found for this postcode.")
+            return
 
-        print(f"\n{formatted['name']}")
-        print(f"Cuisines: {formatted['cuisines']}")
-        print(f"Rating: {formatted['rating']}")
-        print(f"Address: {formatted['address']}")
+        top_10 = restaurants[:10]
+
+        for restaurant in top_10:
+            formatted = format_restaurant(restaurant)
+
+            print(f"\n{formatted['name']}")
+            print(f"Cuisines: {formatted['cuisines']}")
+            print(f"Rating: {formatted['rating']}")
+            print(f"Address: {formatted['address']}")
+
+    except Exception as e:
+        print("Error:", e)
 
 if __name__ == "__main__":
     main()
